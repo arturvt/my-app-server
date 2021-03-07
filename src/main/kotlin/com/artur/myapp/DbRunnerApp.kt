5a779cb5763d10719507213cfecb60bf3acaa9d5
Artur Tenorio <arturvt@gmail.com>
2021-03-07 17:42:04 +0100

    improve logs

--- src/main/kotlin/com/artur/myapp/DbRunnerApp.kt
+++ src/main/kotlin/com/artur/myapp/DbRunnerApp.kt
@@ -10,6 +10,7 @@ import com.artur.myapp.repository.country.CountryRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
@@ -17,6 +18,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 class DbRunnerApp : CommandLineRunner {
 
     private val logger: Logger = LoggerFactory.getLogger(DbRunnerApp::class.java)
+
+    @Value("\${spring.data.mongodb.uri}")
+    val mongoURL: String = ""
 
     @Autowired
     private lateinit var countryRepository: CountryRepository
@@ -27,14 +31,18 @@ class DbRunnerApp : CommandLineRunner {
     @Autowired
     private lateinit var regionFullRepository: RegionFullRepository
 
-
     override fun run(vararg args: String?) {
         logger.info("-------------------------------")
         logger.info("Runner Code")
         logger.info("#####")
-//        fillFullRegions()  // Último País que parou é CH
-
+        checkDb()
         logger.info("Done request")
+    }
+
+    fun checkDb() {
+        logger.info("Checking db connection")
+        logger.info("MongoHost: $mongoURL")
+        logger.info("Number of items countryRepo: ${countryRepository.count()}")
     }
 
     fun fillCountries() {
